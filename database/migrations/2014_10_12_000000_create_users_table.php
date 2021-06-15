37d42a7be4bc717957d5024d2cc7c48f60855455
blackbullerk123 <65282322+blackbullerk123@users.noreply.github.com>
2021-06-15 23:03:47 +0700

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable;
+            $table->string('facebook_link')->nullable;
+            $table->string('provider')->nullable;
+            $table->string('provider_id')->nullable;
+            $table->string('avatar')->nullable;
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_06_15_155533_create_package_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->string('package_name')->nullable();
+            $table->string('package_price')->nullable();
+            $table->string('point_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package');
+    }
+}
