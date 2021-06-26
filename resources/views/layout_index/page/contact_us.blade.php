8b15730af7b1c63115be9d53a442d9df68680ae7
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-06-26 21:22:29 +0700

    update

--- resources/views/layout_index/page/contact_us.blade.php
+++ resources/views/layout_index/page/contact_us.blade.php
@@ -10,36 +10,36 @@
         <div class="col-lg-12">
             <div class="col-lg-4 text-center" style="float: left">
                 <span class="avatar avatar-md mb-3">
-                    <a href="#"><img src="https://gamemobile.io/assets/uploads/1618905948.png" style="height: 200px; width: 200px"
+                    <a href="https://www.facebook.com/kyru.vu/" target="_blank"><img src="{{ asset('images/boy.jpg') }}" style="height: 200px; width: 200px"
                         class="image rounded-circle"></a>
                 </span><br>
                 <div style="margin-top: 20px">
-                    <a  href="#" class="mb-0" style="text-align: center">Surplus Points</a>
+                    <a href="https://www.facebook.com/kyru.vu/" target="_blank" class="mb-0" style="text-align: center">KyRu Vũ</a>
                 </div><br>
             </div>
             <div class="col-lg-4 text-center" style="float: left">
                 <span class="avatar avatar-md mb-3">
-                    <a href="#"><img src="https://gamemobile.io/assets/uploads/1618905948.png" style="height: 200px; width: 200px"
+                    <a href="#" target="_blank"><img src="https://gamemobile.io/assets/uploads/1618905948.png" style="height: 200px; width: 200px"
                         class="image rounded-circle"></a>
                 </span><br>
                 <div style="margin-top: 20px">
-                    <a  href="#" class="mb-0" style="text-align: center">Surplus Points</a>
+                    <a  href="#" target="_blank" class="mb-0" style="text-align: center">Name</a>
                 </div><br>
             </div>
             <div class="col-lg-4 text-center" style="float: left">
                 <span class="avatar avatar-md mb-3">
-                    <a href="#"><img src="https://gamemobile.io/assets/uploads/1618905948.png" style="height: 200px; width: 200px"
+                    <a href="#" target="_blank"><img src="https://gamemobile.io/assets/uploads/1618905948.png" style="height: 200px; width: 200px"
                         class="image rounded-circle"></a>
                 </span><br>
                 <div style="margin-top: 20px">
-                    <a  href="#" class="mb-0" style="text-align: center">Surplus Points</a>
+                    <a  href="#" target="_blank" class="mb-0" style="text-align: center">Name</a>
                 </div><br>
             </div>
         </div>
         <br>
             <div class="row">
                 <div class="col-12 " style="text-align: center">
-                    <h3 class="text-main-4">Phản hồi khách hàng xin vui lòng gửi về email: <b>contact@gamemobile.io</b>
+                    <h3 class="text-main-4">Phản hồi khách hàng xin vui lòng gửi về email: <b>admin@gmail.com</b>
                     </h3> <br>
                 </div>
             </div>
