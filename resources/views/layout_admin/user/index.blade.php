64c54c21b9f0d2d5a99fd53f743dc909aa9730b5
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-06-30 00:01:26 +0700

    update nhẹ

--- resources/views/layout_admin/user/index.blade.php
+++ resources/views/layout_admin/user/index.blade.php
@@ -54,9 +54,9 @@
                           @if($user->banned_status == 0)
                           <a href="{{ route('users.banned', $user->id) }}" class="btn btn-danger btn-sign ban"> Khóa</a> 
                           @else
-                          <a href="{{ route('users.unbanned', $user->id) }}" class="btn btn-success btn-sign ban"> Mở khóa</a></td> 
+                          <a href="{{ route('users.unbanned', $user->id) }}" class="btn btn-success btn-sign ban"> Mở khóa</a> 
                           @endif
-                          <a href="{{ route('users.edit', $user->id) }}" style="margin-left: 10px" class="btn btn-primary btn-sign ban"> Update point</a></td>         
+                          <a href="{{ route('users.edit', $user->id) }}" style="margin-left: 10px" class="btn btn-primary btn-sign ban"> Update point</a>       
                         </td>
                       </tr>
                       @endforeach
